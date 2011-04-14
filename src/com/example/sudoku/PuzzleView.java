b4b3c334d4a964213d7105eadda2daaa6491c303
Aaron Rosenberg <=>
2011-04-13 22:47:22 -0400

    starting to set up board

--- src/com/example/sudoku/PuzzleView.java
+++ src/com/example/sudoku/PuzzleView.java
@@ -21,4 +21,22 @@ public class PuzzleView extends View {
 		setFocusable(true);
 		setFocusableInTouchMode(true);
 	}
+
+	private float width;
+	private float height;
+	private int selX;
+	private int selY;
+	private final Rect selRect = new Rect();
+	
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		width = w / 9f;
+		height = h / 9f;
+		getRect(selX, selY, selRect);
+		Log.d(TAG, "onSizeChanged: width " + width + "height " + height);
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+	private void getRect(int x, int y, Rect rect) {
+		rect.set((int) (x * width), (int) (y * height), (int) (x * width + width), (int) (y * height + height));
+	}
 }
